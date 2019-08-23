13a7357a1ddd47e67cf28eaab9762bef4d64d04f
unknown <dehme.armenio@gmail.com>
2019-08-23 05:25:51 -0300

    add 2 clicks

--- src/app/pages/userView/controller.js
+++ src/app/pages/userView/controller.js
@@ -5,9 +5,18 @@ const onclick = () => {
     alert("hakuna matata")
 }
 
+const onclick2 = () => {
+    alert("hakuna matata2")
+}
+
+const clickFunctions = {
+    onclick: onclick,
+    onclick2: onclick2
+}
+
 const Controller = () => {
 
-    return <View onClickFunction={onclick} />
+    return <View onClickFunction={clickFunctions} />
 }
 
 export default Controller
--- src/app/pages/userView/view.jsx
+++ src/app/pages/userView/view.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const UserView = (props) => {
-    return (<p onClick={props.onClickFunction}>user view</p>)
+    return (
+        <div>
+            <p onClick={props.onClickFunction.onclick}>user view</p>
+            <p onClick={props.onClickFunction.onclick2}> user click 2</p>
+        </div>
+    )
 }
 
 export default UserView
